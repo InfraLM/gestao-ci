fadc456438780562e7c34b241c54a1d277170c37
Vinilean <vinileann@gmail.com>
2026-03-06 14:37:00 -0300

    mudanças34

--- api/index.js
+++ api/index.js
@@ -1,6 +1,9 @@
 // Vercel Serverless Entry Point
 // Routes /api/* requests to the Express app
 
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+
 const app = require('../backend/src/app');
 
-module.exports = app;
+export default app;
